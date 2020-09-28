e84795756eb08c0ebd91c589729329dad510e2aa
HP-Pro6300 <1075605131@qq.com>
2020-09-28 15:38:46 +0800

    'upload server_stream file'

--- raspberryPi/server_stream.py
+++ raspberryPi/server_stream.py
@@ -1,60 +1,77 @@
-from time import sleep
-from zlib import decompress
-from threading import Thread
 import socket
+import time
 import cv2
-# BUFFER_SIZE = 6
-data = []
+import numpy
+from raspberryPi.video_stream import process_img
 
 
-def show_image(image_bytes, image_size):
+def process_img2(img):
+    # 高斯滤波核大小
+    blur_ksize = 5
+    # Canny边缘检测高低阈值
+    canny_lth = 50
+    canny_hth = 150
+    # 1. 灰度化、滤波和Canny
+    # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    blur_gray = cv2.GaussianBlur(img, (blur_ksize, blur_ksize), 1)
+    edges = cv2.Canny(blur_gray, canny_lth, canny_hth)
+    return edges
 
-    global im
-    # try:
-    cv2.imshow("frame", image_bytes)
 
-
-def recv_image():
-    global receiving, im
-    s = socket.socket()
-    s.bind(('192.168.3.55', 8001))
+def ReceiveVideo():
+    # IP地址'0.0.0.0'为等待客户端连接
+    address = ('0.0.0.0', 8003)
+    # 建立socket对象，参数意义见https://blog.csdn.net/rebelqsp/article/details/22109925
+    # socket.AF_INET：服务器之间网络通信
+    # socket.SOCK_STREAM：流式socket , for TCP
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 将套接字绑定到地址, 在AF_INET下,以元组（host,port）的形式表示地址.
+    s.bind(address)
+    # 开始监听TCP传入连接。参数指定在拒绝连接之前，操作系统可以挂起的最大连接数量。该值至少为1，大部分应用程序设为5就可以了。
     s.listen(1)
 
-    while receiving:
-        while receiving:
-            chunk, _ = s.recv(640*480)
+    def recvall(sock, count):
+        buf = b''  # buf是一个byte类型
+        while count:
+            # 接受TCP套接字的数据。数据以字符串形式返回，count指定要接收的最大数据量.
+            newbuf = sock.recv(count)
+            if not newbuf: return None
+            buf += newbuf
+            count -= len(newbuf)
+        return buf
 
-            if chunk == b'start':
-                break
-            elif chunk == b'close':
-                sleep(0.1)
-        else:
+    # 接受TCP连接并返回（conn,address）,其中conn是新的套接字对象，可以用来接收和发送数据。addr是连接客户端的地址。
+    # 没有连接则等待有连接
+    conn, addr = s.accept()
+    print('connect from:' + str(addr))
+    while 1:
+        # start = time.time()  # 用于计算帧率信息
+        length = recvall(conn, 16)  # 获得图片文件的长度,16代表获取长度
+        stringData = recvall(conn, int(length))  # 根据获得的文件长度，获取图片文件
+        data = numpy.frombuffer(stringData, numpy.uint8)  # 将获取到的字符流数据转换成1维数组
+        # decimg = cv2.imdecode(data, cv2.IMREAD_COLOR)  # 将数组解码成图像
+        decimg = cv2.imdecode(data, cv2.IMREAD_GRAYSCALE)  # 将数组解码成图像
+        # print(decimg.shape)
+        process_img(decimg)
+        cv2.imshow("ss", process_img2(decimg))
+        cv2.imshow('SERVER', decimg)  # 显示图像
+
+        # 进行下一步处理
+        # 。
+        # 。
+        # 。
+
+        # 将帧率信息回传，主要目的是测试可以双向通信
+        # end = time.time()
+        # seconds = end - start
+        # fps = 1 / seconds
+        # conn.send(bytes(str(int(fps)), encoding='utf-8'))
+        k = cv2.waitKey(10) & 0xff
+        if k == 27:
             break
-        while receiving:
-            chunk, _ = s.recv(480*640)
-            if chunk.startwith(b'_over'):
-                image_size = eval(chunk[5:])
-                try:
-                    image_data = decompress(b''.join(data))
-                except:
-                    data.clear()
-                    break
-                global thread_show
-                thread_show = Thread(target=show_image, args=(image_data, image_size))
-                thread_show.daemon =True
-                thread_show.start()
-                data.clear()
-                break
+    s.close()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-
-    receiving = True
-    recv_image()
-    # thread_sender = Thread(target=recv_image)
-    # thread_sender.daemon = True
-    # thread_sender.start()
-
-
-
-
+    ReceiveVideo()
--- raspberryPi/video_stream.py
+++ raspberryPi/video_stream.py
@@ -37,8 +37,8 @@ class VideoStreamingTest(object):
                     jpg = stream_bytes[first:last + 2]
                     stream_bytes = stream_bytes[last + 2:]
                     image = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-                    new_screen = process_img(image)
-                    cv2.imshow('window', new_screen)
+                    # new_screen = process_img(image)
+                    # cv2.imshow('window', new_screen)
                     cv2.imshow('image', image)
 
                     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -65,10 +65,11 @@ def draw_lines(img, lines):
 
 
 def process_img(original_image):
-    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)  # 灰度化
-    processed_img = cv2.Canny(processed_img, threshold1=200, threshold2=300)  # 边缘特征
+    # processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)  # 灰度化
+    processed_img = cv2.Canny(original_image, threshold1=200, threshold2=300)  # 边缘特征
     processed_img = cv2.GaussianBlur(processed_img, (3, 3), 0)  # 高斯模糊
-    vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]], np.int32)
+    cv2.imshow("ssaa", processed_img)
+    vertices = np.array([[10, 500], [10, 300], [300, 200], [500, 200], [800, 300], [800, 500]], np.int32) # 坐标自己标定
     processed_img = roi(processed_img, [vertices])  # 不规则ROI区域截取
 
     #                       edges
